95b46a4bda04c93250aeb6ad0eb6d8ac52b78a3e
Adam DuQuette <duquetteadam@gmail.com>
2018-03-21 18:13:48 -0400

    Add create template, action, and route for ArticleController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :articles
+  
+  #get 'articles/new', to: 'articles#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
