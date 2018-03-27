6937a3103ef5c4713d241facc13c10c1d568745d
Adam DuQuette <duquetteadam@gmail.com>
2018-03-27 18:50:58 -0400

    Signup users from UI

---
+++ db/migrate/20180327224813_fix_password_digest_attr_name.rb
@@ -0,0 +1,5 @@
+class FixPasswordDigestAttrName < ActiveRecord::Migration
+  def change
+    rename_column :users, :password_disgest, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327215341) do
+ActiveRecord::Schema.define(version: 20180327224813) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180327215341) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_disgest"
+    t.string   "password_digest"
   end
 
 end
